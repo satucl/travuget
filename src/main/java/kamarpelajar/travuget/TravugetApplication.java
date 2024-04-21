018d8ecee4db516aca98bbd3b4307d7dc80f9668
Xave Senduk <fransiscusxave@gmail.com>
2024-04-21 21:21:46 +0800

    init springboot

---
+++ src/main/java/kamarpelajar/travuget/TravugetApplication.java
@@ -0,0 +1,12 @@
+package kamarpelajar.travuget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravugetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravugetApplication.class, args);
+	}
+}
---
+++ src/test/java/kamarpelajar/travuget/TravugetApplicationTests.java
@@ -0,0 +1,13 @@
+package kamarpelajar.travuget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravugetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
